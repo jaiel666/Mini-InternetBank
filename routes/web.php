fc8b4a53be5e2945d06b99318bc6d940872738b4
jaiel666 <mgusts23@gmail.com>
2024-01-05 15:25:16 +0200

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -13,40 +13,27 @@ Route::get('/', function () {
 
 Route::get('/register', [RegistrationController::class, 'create'])->name('registration.create');
 Route::post('/register', [RegistrationController::class, 'store'])->name('registration.store');
+
 Route::get('/login', [LoginController::class, 'create'])->name('login.create');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+
 Route::get('/user_main_page/logout', [UserMainPageController::class, 'logout'])->name('user_main_page.logout');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/user-main-page', [UserMainPageController::class, 'index'])->name('user_main_page');
+    Route::get('/profile', [UserMainPageController::class, 'profile'])->name('user_main_page.profile');
+    Route::get('/cryptocurrencies/buy', [CryptocurrencyController::class, 'showBuyForm'])->name('cryptocurrencies.buy');
+    Route::post('/cryptocurrencies/buy', [CryptocurrencyController::class, 'buyCrypto'])->name('cryptocurrencies.buy.post');
+    Route::get('/cryptocurrencies/sell', [CryptocurrencyController::class, 'sellPage'])->name('cryptocurrencies.sell');
+    Route::post('/cryptocurrencies/sell', [CryptocurrencyController::class, 'sell'])->name('cryptocurrencies.sell.post');
+    Route::get('/user-main-page/transfer', [UserMainPageController::class, 'showTransferForm'])->name('user_main_page.transfer');
+    Route::post('/user-main-page/transfer', [UserMainPageController::class, 'transferFunds'])->name('user_main_page.transfer_funds');
+    Route::get('/user-main-page/transfer-history', [UserMainPageController::class, 'showTransferHistory'])->name('user_main_page.transfer_history');
+    Route::get('/investment-accounts/create', [InvestmentAccountController::class, 'create'])->name('investment.create');
+    Route::post('/investment-accounts', [InvestmentAccountController::class, 'store'])->name('investment.store');
+    Route::get('/investment-accounts', [InvestmentAccountController::class, 'index'])->name('investment.index');
+    Route::get('/investment/history', [InvestmentAccountController::class, 'investmentHistory'])->name('investment.history');
+    Route::get('/income-history', [UserMainPageController::class, 'incomeHistory'])->name('user_main_page.income_history');
 });
 
-Route::get('/user-main-page/transfer', [UserMainPageController::class, 'showTransferForm'])->name('user_main_page.transfer');
-Route::post('/user-main-page/transfer', [UserMainPageController::class, 'transferFunds'])->name('user_main_page.transfer_funds');
-Route::get('/user-main-page/transfer-history', [UserMainPageController::class, 'showTransferHistory'])->name('user_main_page.transfer_history');
-
-
-
-Route::get('/cryptocurrencies', [CryptocurrencyController::class, 'index'])
-    ->name('cryptocurrencies.index');
-
-Route::get('/profile', [UserMainPageController::class, 'profile'])
-    ->name('user_main_page.profile')
-    ->middleware('auth');
-
-Route::get('/cryptocurrencies/buy', [CryptocurrencyController::class, 'showBuyForm'])->name('cryptocurrencies.buy');
-Route::post('/cryptocurrencies/buy', [CryptocurrencyController::class, 'buyCrypto'])->name('cryptocurrencies.buy.post');
-
-Route::get('/cryptocurrencies/sell', [CryptocurrencyController::class, 'sellPage'])->name('cryptocurrencies.sell');
-Route::post('/cryptocurrencies/sell', [CryptocurrencyController::class, 'sell'])->name('cryptocurrencies.sell.post');
-
-
-Route::get('/investment-accounts/create', [InvestmentAccountController::class, 'create'])->name('investment.create');
-
-Route::post('/investment-accounts', [InvestmentAccountController::class, 'store'])->name('investment.store');
-
-Route::get('/investment-accounts', [InvestmentAccountController::class, 'index'])->name('investment.index');
-Route::get('/investment/history', [InvestmentAccountController::class, 'investmentHistory'])->name('investment.history');
-Route::get('/income-history', [UserMainPageController::class, 'incomeHistory'])->name('user_main_page.income_history');
-
-
+Route::get('/cryptocurrencies', [CryptocurrencyController::class, 'index'])->name('cryptocurrencies.index');
